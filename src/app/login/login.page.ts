df17715ae047ef8d777a9770ffeeccf93aa1deb5
TarekBakr <tarek@dhaman.org>
2021-06-29 14:20:12 +0300

    login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Storage } from '@capacitor/storage';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AuthService } from '../shared/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  lang='en';
+  toggled=true;
+  form;
+  constructor(private toast: ToastController, private authService: AuthService, private loadingCtrl: LoadingController) {}
 
   ngOnInit() {
+     this.form = new FormGroup({
+      username: new FormControl('',[Validators.email, Validators.required])
+      ,password: new FormControl('',[Validators.required])
+    });
   }
 
+  login(){
+    this.loadingCtrl.create({
+      message: 'Log in .... please wait'
+    }).then(loadingElmnt=>{
+      loadingElmnt.present();
+      this.authService.authLogin(this.username.value,this.password.value).subscribe(loginResponse=>{
+        loadingElmnt.dismiss();
+        console.log(loginResponse);
+      });
+    });
+  }
+
+  onLangChange() {
+    this.lang = this.toggled?'en':'ar';
+    Storage.set({ key: 'lang', value: this.lang }).then((data) => {
+     console.log(data);
+     console.log(this.username.value);
+    });
+  }
+
+get username(){
+  return this.form.get('username');
+}
+
+get password(){
+  return this.form.get('password');
+}
+
+
+
+  private showToast(msg: string) {
+    this.toast
+      .create({
+        message: msg,
+        position: 'middle',
+        duration: 1000,
+      })
+      .then((toastCtrl) => {
+        toastCtrl.present();
+      });
+  }
 }
