4254104b1c517f1945bf51d196b45fe8c827e808
TarekBakr <tarek@dhaman.org>
2021-06-30 11:08:07 +0300

    login.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Storage } from '@capacitor/storage';
 import { LoadingController, ToastController } from '@ionic/angular';
 import { AuthService } from '../shared/auth.service';
@@ -9,47 +10,54 @@ import { AuthService } from '../shared/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  lang='en';
-  toggled=true;
+  lang = 'en';
+  toggled = true;
   form;
-  constructor(private toast: ToastController, private authService: AuthService, private loadingCtrl: LoadingController) {}
+  constructor(
+    private toast: ToastController,
+    private authService: AuthService,
+    private loadingCtrl: LoadingController,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-     this.form = new FormGroup({
-      username: new FormControl('',[Validators.email, Validators.required])
-      ,password: new FormControl('',[Validators.required])
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required]),
     });
   }
 
-  login(){
-    this.loadingCtrl.create({
-      message: 'Log in .... please wait'
-    }).then(loadingElmnt=>{
-      loadingElmnt.present();
-      this.authService.authLogin(this.username.value,this.password.value).subscribe(loginResponse=>{
-        loadingElmnt.dismiss();
-        console.log(loginResponse);
+  login() {
+    this.loadingCtrl
+      .create({
+        message: 'Log in .... please wait',
+      })
+      .then((loadingElmnt) => {
+        loadingElmnt.present();
+        this.authService
+          .authLogin(this.username.value, this.password.value)
+          .subscribe((loginResponse) => {
+            loadingElmnt.dismiss();
+            console.log(loginResponse);
+          });
       });
-    });
   }
 
   onLangChange() {
-    this.lang = this.toggled?'en':'ar';
+    this.lang = this.toggled ? 'en' : 'ar';
     Storage.set({ key: 'lang', value: this.lang }).then((data) => {
-     console.log(data);
-     console.log(this.username.value);
+      console.log(data);
+      console.log(this.username.value);
     });
   }
 
-get username(){
-  return this.form.get('username');
-}
+  get username() {
+    return this.form.get('username');
+  }
 
-get password(){
-  return this.form.get('password');
-}
-
-
+  get password() {
+    return this.form.get('password');
+  }
 
   private showToast(msg: string) {
     this.toast
